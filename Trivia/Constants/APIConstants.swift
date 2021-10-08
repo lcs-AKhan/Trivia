95ea7dbbae5a45bcfdec1084da929ccee651dad9
Abdul Khan <akhan@lcs.on.ca>
2021-10-07 20:24:23 -0400

    Added APIConstant

---
+++ Trivia/Constants/APIConstants.swift
@@ -0,0 +1,12 @@
+//
+//  APIConstants.swift
+//  Trivia
+//
+//  Created by Abdul Ahad Khan on 2021-10-07.
+//
+
+import Foundation
+
+enum APIConstants {
+    static let baseUrl = "https://opentdb.com/api.php?amount=1"
+}
---
+++ Trivia/Feature/TrivaQuestions/Model/Trivia.swift
@@ -0,0 +1,31 @@
+//
+//  Trivia.swift
+//  Trivia
+//
+//  Created by Abdul Ahad Khan on 2021-10-07.
+//
+
+import Foundation
+
+struct Trivia: Decodable {
+    
+}
+
+
+//{
+//   "response_code":0,
+//   "results":[
+//      {
+//         "category":"Entertainment: Board Games",
+//         "type":"multiple",
+//         "difficulty":"medium",
+//         "question":"What special item did the creators of Cards Against Humanity ship for their Black Friday pack?",
+//         "correct_answer":"Bull Feces",
+//         "incorrect_answers":[
+//            "A Card Expansion",
+//            "A Racist Toy",
+//            "Cat Urine"
+//         ]
+//      }
+//   ]
+//}
